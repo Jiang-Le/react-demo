6c6568e8fd901a3f4eace084223de4dda702cf86
lejiang <lejiang@tencent.com>
2024-08-12 20:07:52 +0800

    基础玩法

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
+import { useState } from 'react';
+
 function Square({idx, value, handleSquareClick}) {
   return <button className="square" onClick={()=>handleSquareClick(idx)}>{value}</button>;
 }
 
 function Board({row, col}) {
-  function handleSquareClick(idx) {
-    
+
+  const [squareValues, setSquareValues] = useState(Array(row*col).fill(null))  
+  const [step, setStep] = useState(0)
+
+  const winnerPos = findWinner(squareValues)
+  let winner = null
+  if (winnerPos) {
+    winner = squareValues[winnerPos[0]]
   }
+
+  let state = null
+  if (winner) {
+    state = "Winner: " + winner
+  } else {
+    state = "Next player: " + calculatePlayer(step)
+  }
+
+
+  function handleSquareClick(idx) {
+    if (findWinner(squareValues)) {
+      return
+    }
+    if (squareValues[idx]) {
+      return
+    }
+    const nextSquare = squareValues.slice()
+    nextSquare[idx] = calculatePlayer(step)
+    setStep(step + 1)
+    setSquareValues(nextSquare)
+  }
+
+  function calculatePlayer(currentStep) {
+    return currentStep % 2 === 0 ? 'X' : 'O'
+  }
+
 
   let board = [];
   for (let i = 0; i < row; i++) {
     let cols = [];
     for (let j = 0; j < col; j++) {
-      cols.push(<Square idx={row*col+col} value={null} handleSquareClick={handleSquareClick}/>)
+      let idx = i*col+j
+      let value = squareValues[idx]
+      cols.push(<Square idx={idx} value={value} handleSquareClick={handleSquareClick}/>)
     }
     board.push(<div className="board-row">{cols}</div>)
   }
 
   return <>
+    <div className="status">{state}</div>
     {board}
   </>
 }
 
 export default function Game() {
   return <><Board row={3} col={3}/></>
+}
+
+function findWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return lines[i].slice();
+    }
+  }
+  return null;
 }
